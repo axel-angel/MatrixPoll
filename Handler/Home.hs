34fbd5be6a91d469b12e7094e8683b6386686300
Axel Angel <axel+git@vneko.ch>
2014-09-21 13:49:24 +0200

    Implement poll results display/table/form, Ajax post

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,6 +3,9 @@ module Handler.Home where
 
 import Import
 import Data.Time.Clock (getCurrentTime, utctDay)
+import Data.Time.Calendar (Day)
+import Data.List (sort, group, transpose)
+import GHC.Exts (sortWith)
 
 
 getHomeR :: Handler Html
@@ -14,7 +17,7 @@ postHomeR :: Handler Html
 postHomeR = do
     (title, desc, cols, vals) <- runInputPost pollForm
     liftIO $ print (title, desc, cols, vals)
-    now <- liftIO $ getCurrentTime >>= return . utctDay
+    now <- liftIO getNow
     pid <- runDB . insert $ Poll Nothing title desc cols vals now
     setMessageI $ MsgYouCreated title
     redirect $ SeePollR pid
@@ -23,7 +26,28 @@ postHomeR = do
 getSeePollR :: PollId -> Handler Html
 getSeePollR pid = do
     poll <- runDB $ get404 pid
+    results <- runDB $ selectList [ResultPoll ==. pid] [Asc ResultNickname]
+    let answers = transpose $ map (resultAnswers . entityVal) results
+    let bests = map mostFrequent answers
     defaultLayout $(widgetFile "see_poll")
+
+
+postAjaxResultR :: Handler TypedContent
+postAjaxResultR = do
+    res <- runInputPostResult rowForm
+    case res of
+        FormSuccess (pid, nickname, values) -> do
+            now <- liftIO getNow
+            rid <- runDB . insert $ Result pid nickname values now
+            return . toTypedContent . object $
+                [ "success" .= True
+                , "pid" .= pid
+                , "rid" .= rid
+                , "nickname" .= nickname
+                , "values" .= values
+                ]
+        _ ->
+            return . toTypedContent . object $ [ "success" .= False ]
 
 
 pollForm :: FormInput Handler (Text, Maybe Text, [Text], [Text])
@@ -32,6 +56,19 @@ pollForm = (\x y z u -> (x, y, z, u))
     <*> iopt textField "desc"
     <*> ireq multiNonEmptyField "columns"
     <*> ireq multiNonEmptyField "values"
+
+
+rowForm :: FormInput Handler (PollId, Text, [Text])
+rowForm = (\x y z -> (x, y, z))
+    <$> ireq (selectField pollKeyField) "pid"
+    <*> ireq textField "nickname"
+    <*> ireq multiNonEmptyField "values"
+
+
+pollKeyField :: Handler (OptionList (Key Poll))
+pollKeyField = optionsPersistKey noFilter [] (const msg)
+    where msg = toMessage ("" :: Text)
+          noFilter = []::[Filter Poll]
 
 
 multiNonEmptyField :: Field Handler [Text]
@@ -43,3 +80,16 @@ multiField = Field
     , fieldView = undefined
     , fieldEnctype = UrlEncoded
     }
+
+getNow :: IO Day
+getNow = getCurrentTime >>= return . utctDay
+
+
+mostFrequent :: (Eq a, Ord a) => [a] -> Maybe (a, Int)
+mostFrequent = safeHead . reverse . sortWith (\(v,c) -> c) . countOccurences
+    where safeHead (x:_) = Just x
+          safeHead _ = Nothing
+
+
+countOccurences :: (Eq a, Ord a) => [a] -> [(a, Int)]
+countOccurences = map (\xs@(x:_) -> (x, length xs)) . group . sort
